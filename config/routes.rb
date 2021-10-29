0c2518cf5aedcbde0d5801187d7379cbb019b8d2
hirokikurosawa <kuro16sc@gmail.com>
2021-10-29 04:16:23 +0000

    [Start]admin側のcontrollerの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  namespace :admins do
+    get 'users/index'
+    get 'users/show'
+    get 'users/edit'
+    get 'users/update'
+  end
+  namespace :admins do
+    get 'homes/top'
+  end
   devise_for :users
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
