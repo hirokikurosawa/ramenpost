fce76dd46bec66f3464b71e2d0486adbfee6f525
hirokikurosawa <kuro16sc@gmail.com>
2021-11-01 18:50:40 +0000

    [Add]投稿の削除機能追加

--- app/controllers/users/posts_controller.rb
+++ app/controllers/users/posts_controller.rb
@@ -19,6 +19,9 @@ class Users::PostsController < ApplicationController
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to user_path(current_user.id)
   end
 
   def following_posts
