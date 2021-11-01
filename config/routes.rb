58f0487383496ad75d13916ed6925b4270717892
hirokikurosawa <kuro16sc@gmail.com>
2021-11-01 04:17:41 +0000

    [Add]使用する予定のルーティングを一括追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,37 +1,44 @@
 Rails.application.routes.draw do
-  namespace :users do
-    get 'likes/index'
-    get 'likes/creat'
-    get 'likes/destroy'
-  end
-  namespace :users do
-    get 'posts/index'
-    get 'posts/new'
-    get 'posts/creat'
-    get 'posts/show'
-    get 'posts/destroy'
-    get 'posts/following_posts'
-    get 'posts/search'
-  end
-  namespace :users do
-    get 'users/show'
-    get 'users/check'
-    get 'users/destroy'
-  end
-  namespace :users do
-    get 'hoems/top'
-    get 'hoems/about'
-  end
-  namespace :admins do
-    get 'users/index'
-    get 'users/show'
-    get 'users/edit'
-    get 'users/update'
-  end
-  namespace :admins do
-    get 'homes/top'
-  end
-  devise_for :users
-  devise_for :admins
+  devise_for :users, 
+  controllers: {
+  sessions: 'users/sessions',
+  passwords: 'users/passwords',
+  registrations: 'users/registrations'
+  }
+  
+  devise_for :admins, 
+  controllers: {
+  sessions: 'admins/sessions',
+  passwords: 'admins/passwords',
+  registrations: 'admins/registrations'
+  }
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  scope module: :admins do
+    get '/admins', to: 'homes#top'
+  end
+
+  namespace :admins do
+    resources :users, only: [:index, :show, :edit, :update]
+  end
+
+  scope module: :users do
+    root to: 'homes#top'
+    get '/about' => 'homes#about'
+    resources :users, only: [:show, :destroy] do
+      collection do
+        get :check
+      end
+    end
+    resources :posts, only: [:index, :new, :create, :show, :destroy] do
+      collection do
+        get :following_posts
+        get :search
+      end
+    end
+    resources :relationships, only: [:index, :create, :destroy]
+    resources :likes, only: [:index, :create, :destroy]
+  end
+  
 end
