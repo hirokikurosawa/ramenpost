b09a78ea5b21877da8f6cc0a8505e8fe48e5c137
hirokikurosawa <kuro16sc@gmail.com>
2021-11-01 18:23:48 +0000

    [Add]投稿一覧機能追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'e29758f186b652e3faae6904792514ee4cf20015731469e72f85be9657d332aa51fda863aeb2c51539550e8fca10064b714059a85d42fc14f3b048e36d34f91f'
