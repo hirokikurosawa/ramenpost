74dca943d29a4b4b5ae656c168ea54866971901f
hirokikurosawa <kuro16sc@gmail.com>
2021-11-04 20:59:06 +0000

    [Add]管理者側のページリンク追加

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -4,11 +4,22 @@ class Admins::UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to admins_user(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :encrypted_password, :nickname, :icon, :introduction)
   end
 end
