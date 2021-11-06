d87c30d05414344cc804a5ed1868871180a27b4c
hirokikurosawa <kuro16sc@gmail.com>
2021-11-06 02:23:58 +0000

    [Add]コメント追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   attachment :post_image
   
+  #検索機能のメソッド
   def self.search(keyword)
     where(["introduction like?",  "%#{keyword}%"])
   end
