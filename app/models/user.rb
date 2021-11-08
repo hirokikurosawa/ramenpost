32975c5e6c5f4d4de4e11123083a67a8df16bda7
hirokikurosawa <kuro16sc@gmail.com>
2021-11-08 00:13:59 +0000

    [Fix]ユーザーのアイコンを保存、表示されるように修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,21 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
+  mount_uploader :icon, ImageUploader
+
   has_many :posts, dependent: :destroy
   has_many :relationships, dependent: :destroy, foreign_key: :following_id
   has_many :likes, dependent: :destroy
-  
+
   #フォロー機能のためのアソシエーション
   has_many :followings, through: :relationships, source: :follower
   has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: :follower_id
   has_many :followers, through: :reverse_of_relationships, source: :following
-  
+
   #フォロー機能のメソッド
   def is_followed_by?(user)
     reverse_of_relationships.find_by(following_id: user.id).present?
   end
-  
+
   #いいね機能のメソッド
   def already_liked?(post)
     self.likes.exists?(post_id: post.id)
